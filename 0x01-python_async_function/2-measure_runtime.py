a517c1fc28ae3ba4571e4dfc1117cf95b46a1db0
xeno <xeno@xeno.0.2.15>
2024-04-22 15:01:14 +0100

    async

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -3,12 +3,17 @@
 import asyncio
 import time
 
-
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
-
 def measure_time(n: int, max_delay: int) -> float:
-   "computes the average runtime"
+    "computes the average runtime"
     start_time = time.time()
     asyncio.run(wait_n(n, max_delay))
-    return (time.time() - start_time) / n
+    end_time = time.time()
+    total_time = end_time - start_time
+    return total_time / n
+
+if __name__ == "__main__":
+    n = 5
+    max_delay = 9
+    print(measure_time(n, max_delay))
