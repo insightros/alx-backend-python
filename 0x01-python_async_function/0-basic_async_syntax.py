a8bc8f5b018dd06454216cd6dc987bdac38cf50e
xeno <xeno@xeno.0.2.15>
2024-04-22 14:52:57 +0100

    async

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,6 +5,7 @@ import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
+    "waits for an amound of seconds"
     wait_time = random.random() * max_delay
     await asyncio.sleep(wait_time)
     return wait_time
